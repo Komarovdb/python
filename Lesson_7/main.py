c3bd04c80c55692ab8f13a9f918c050a89d006ea
movavi_school <selleran@list.ru>
2022-10-30 16:32:46 +0700

    30th of october

--- Lesson_7/main.py
+++ Lesson_7/main.py
@@ -17,4 +17,26 @@
 #if 5 < 0
 #amogus=90-10
 #print(amobus)
-
+#try:
+    #name = input("богом дан:")
+    #if name == "Антон":
+        raise Exception("НЕА")
+# except Exception as error_m:
+#     print("suuuuiiiiii", error_m)
+# else:
+#     print("Ну вот так можнл")
+# finally:
+#     print("ааг")
+# try:
+#     number = int(input())
+#     x = 0.1377388885921/number
+# except Exception:
+#     pass
+# if 3==3:
+#     pass#TODO:не забудь
+# temp = 365
+# print    (temp)
+def summa(numbers):
+    return sum(numbers)
+print(summa([1,2,3]))
+assert summa([1,2])=4
---
+++ lesson_8/main.py
@@ -0,0 +1,41 @@
+import random as r
+import turtle
+#print(r.randint(0,100))
+#from random import randint #only randint for random
+# lst = [0, 1, 2, 3, 4, 5, 6, 7]
+# print(r.choice(lst)) #выбор содержимого
+# r.shuffle(lst) #shuffle=перемешать содержимое
+# print(lst)
+import turtle as t
+screen=turtle.Screen()
+t = t.Turtle()
+# t.forward(500)
+t.pensize(2)
+t.speed(0)
+t.shape("turtle")
+t.color("blue")
+x = 100
+y = 200
+t.fd(x)
+t.rt(90)
+t.fd(y)
+t.rt(90)
+t.fd(x)
+t.rt(90)
+t.fd(y)
+t.rt(90)
+t.penup()
+t.fd(20)
+t.rt(90)
+t.fd(20)
+t.pendown()
+t.fd(30)
+t.rt(-90)
+t.fd(60)
+t.rt(-90)
+t.fd(30)
+t.rt(-90)
+t.fd(60)
+t.circle(30)
+t.write("Movavi", font=("Arial black", 50,"normal"), align="center")
+screen.exitonclick()
