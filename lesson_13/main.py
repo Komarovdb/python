5a170eaa232ca572f1c166e301f66e1369ce0030
movavi_school <Kovalenkoaa2008@icloud.com>
2022-12-18 16:27:38 +0700

    18.12.22

--- lesson_13/main.py
+++ lesson_13/main.py
@@ -23,12 +23,53 @@
 # l3=list(set(l1) & set(l2))
 # l3.sort(reverse=True)
 # print(l3[::-1])
-ty=[6,2,3,4,4,6,7,4,9,2]
-tp=[]
-x=0
-for x in range(0, len(ty)):
-    if ty[x] in tp:
-        print("YES")
-    else:
-        print("NO")
-        tp.append(ty[x])
+# ty=[6,2,3,4,4,6,7,4,9,2]
+# tp=[]
+# x=0
+# for x in range(0, len(ty)):
+#     if ty[x] in tp:
+#         print("YES")
+#     else:
+#         print("NO")
+#         tp.append(ty[x])
+# dict1={"key1": 1
+#        } #словарь
+# empty_dictionary0 = {} #пустой словарь 1
+# empty_dictionary1 = dict() #пустой словарь 2
+# print(dict1["key1"])
+# x = 5
+# while x == 4:
+#     pass #gt 1030
+# while True:
+#     print("infinity")
+# phrase = "пхрасе"
+# for supra in phrase:
+#     print(supra)
+# for jojo in range(1,5):
+#     print(jojo)
+# set1 = dict()
+# set = {1,2,3,2,3,3,3}
+# print(set)
+# set0={1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2}
+set1={1,2,3,4,5}
+set2={3,4,5,6,7}
+# print(set0)
+# set3={"A","B","C"}
+# print(set3)
+# set000={1,[2,3]}
+# print(set000) #X
+# print(set1.union(set2))
+# print(set1 | set2)
+# print(set1.intersection(set2))
+# print(set1 & set2)
+# print(set1.difference(set2))
+# print(set1 - set2)
+# print(set1.symmetric_difference(set2))
+# print(set1^set2)
+# from random import randint
+# lst = []
+# for _ in range(5):
+#     lst.append(randint(1,5))
+# print(lst)
+# unique = set(lst)
+# print(f"{len(unique)} {unique}")
