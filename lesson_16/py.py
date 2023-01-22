766cd66fe9628572f36fbb318618ed8ba7ace9c3
movavi_school <alexomger@mail.ru>
2023-01-22 16:28:13 +0700

    ''

---
+++ lesson_16/py.py
@@ -0,0 +1,69 @@
+
+# o1=lambda a, p: a+p+1
+# print(o1(4,6))
+# ans="d"
+# exit_trigger = lambda yn: True if yn == "d" else False
+#
+# #list comrehesion
+# lst = []
+# lst.append
+# for i in range (1,6):
+#     lst.append(i)
+# print(lst)
+# st = [i for i in range(1,6)]
+
+#1
+
+# c2f = lambda c: 9/5 * c + 32
+# f2c = lambda f:(f-32)* 5/9
+# c2k = lambda c: 273.15 + c
+# k2c = lambda k: k - 273.15
+# f2k = lambda f: c2k(f2c(f))
+# k2f(f2c(k))
+
+#2
+import random as r
+# snickers_v_pene=int(input("ckok o/pocaem")) + 1
+# dices = [i for i in range (snickers_v_pene)]
+# print(dices)
+# exit=lambda yn: True if yn == "d" else False
+# pl=input("xotewb BbIйTu? 1-yes 0-no")
+# print(exit(pl))
+# if pl == True:
+#     break
+
+#3
+# chars = [list("абвгдеёжзийклмнопрстуфхцчшщъыьэюя"),
+# #          list("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"),
+# #          list("abcdefghijklmnopqrstuvwxyz"),
+# #          list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+# #          list("1234567890")
+# #          ]
+# # lst = []
+# # for i in range(6):
+# #    lst.append(r.choice(r.choice(chars)))
+# # c = "".join(lst)
+# # ssilka = input("дай ссылОЧКУ   ")
+# # mtdict = {}
+# # if ssilka in mtdict:
+# #     print("эта ссылка уже есть, вот её код: ",mtdict[ssilka])
+# # else:
+# #     mtdict[ssilka] = c
+# #     print(mtdict[ssilka])
+
+
+#4
+
+a=10
+b=40
+lambda a,b: b - a
+priny(yy1(a,b))
+yy2 = lambda a,b : b + a
+yy3 = lambda a,b : b / a
+yy4 = lambda a,b : b * a
+yy5 = lambda a,b : b / a
+lambda x: -x if x<0 else x
+kriperkrutoy2014 = lambda x : -x if x>0 else x
+print(kriperkrutoy2014(-5))
+lgrm = lambda a, b : mat.log(b, a)
+print(lgrm(2, 1024))
